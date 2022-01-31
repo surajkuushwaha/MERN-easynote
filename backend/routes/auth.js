7c1d9c9d08b6f1a3c3a68536bda7388cfd113dd3
Suraj kushwaha <skk74143@gmail.com>
2022-02-01 01:08:51 +0530

    create a collection in thunder client and added exception handling while calling auth api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,24 +3,37 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-// create a user using: POST "/api/auth/" doesn't require auth
+// create a user using: POST "/api/auth/createUser" . No login required
 
-router.post('/', [
+router.post('/createUser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('password', 'Enter a valid password').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail()
-], (req, res) => {
+], async (req, res) => {
+    // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-    }).then(user => res.json(user))
-    .catch(err=> {console.log(err)
-        res.json({error: 'Please enter a unique value for email', message: err.message})})
-    // res.send(req.body);
+    // checks whether the user with this email exists already
+    try {
+
+        let user = await User.findOne({ email: req.body.email });
+        console.log()
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" });
+        }
+
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+        })
+        res.json(user)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occured");
+
+    }
 })
 module.exports = router
