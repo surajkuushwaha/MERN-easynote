3c782791dabbfd2ae4b72f4f03765dedeb081655
Suraj kushwaha <skk74143@gmail.com>
2022-01-31 23:01:46 +0530

    Added express-validator and validated post data and added res regurding validation

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+const mongoURL = 'mongodb://localhost:27017/easynote?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURL,()=>{
