f5990bd90c3d2aa23ddcb4c9fcde2c92b5c6afbf
Suraj kushwaha <skk74143@gmail.com>
2022-02-02 00:58:47 +0530

    Added PUT request for update

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const fetchuser = require('../middleware/fetchuser');
 
 
-//ROUTE 1: Get all the notes: GET "/api/auth/createUser" . No login required
+//ROUTE 1: Get all the notes: GET "/api/notes/fetchAllNotes" . No login required
 router.get('/fetchAllNotes', fetchuser, async (req, res) => {
     try {
         const note = await Note.find({ user: req.user.id });
@@ -16,7 +16,7 @@ router.get('/fetchAllNotes', fetchuser, async (req, res) => {
     }
 })
 
-//ROUTE 2: Add a new notes using: POSST "/api/auth/addnote" . No login required
+//ROUTE 2: Add a new notes using: POSST "/api/notes/addnote" . login required
 router.post('/addnote', fetchuser, [
     body('title', 'Enter a valid title').isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 })
@@ -39,4 +39,32 @@ router.post('/addnote', fetchuser, [
         res.status(500).send("Internal server error");
     }
 })
+
+//ROUTE 3: update an existion note using : POST "/api/notes/addnote" . login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        // Create a newnote object
+        const newNote={};
+        if(title){newNote.title = title};
+        if(description){newNote.description = description};
+        if(tag){newNote.tag = tag};
+
+        // Find the note to be updated
+        let note = await Note.findById(req.params.id);
+        if(!note){res.status(404).send("Not found")};
+
+        if(note.user.toString()!== req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+        res.json({note});
+        
+    } catch (error) {
+        console.log(error.message+"here");
+        res.status(500).send("Internal server error");
+    }
+})
+
+
 module.exports = router
