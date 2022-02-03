52d8b3e143771f801d2886dd633014af8310b229
Suraj kushwaha <skk74143@gmail.com>
2022-02-03 22:34:54 +0530

    added active link functionality using useLocation of router module

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,7 @@
 import React,{useContext,useEffect} from 'react';
-import noteContext from '../context/Notes/noteContext';
 
 export const About = () => {
-  const a = useContext(noteContext);
-  useEffect(()=>{
-    a.update()
-    // eslint-disable-next-line
-  },[])
   return <div>
-      <h1>this is about us {a.state.name} {a.state.class}</h1>
+      <h1>this is about page</h1>
   </div>;
 };
--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -1,24 +1,9 @@
 import NoteContext from "./noteContext";
-import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Suraj",
-        "class": "5b"
-    };
-    const [state, setstate] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setstate({
-                "name": "suman",
-                "class": "68"
-            })
-        }, 1000)
-    }
-
 
     return (
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
