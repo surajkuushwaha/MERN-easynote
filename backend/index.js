ad54b70ba9b68bd58459d8a15289a919cca4cd62
Suraj kushwaha <skk74143@gmail.com>
2022-02-06 01:00:39 +0530

    Added Fetch and added edit modal -66

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,20 @@
 const connectToMongo = require('./db')
 const express = require('express')
+var cors = require('cors')
+
 
 connectToMongo();
+
 
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())
 
 //Avalable routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
-
-app.get('/', (req, res) => {
-  res.send('Hello suraj!')
-})
-
-
 
 app.listen(port, () => {
   console.log(`EasyNote app backend listening at port ${port}`)
