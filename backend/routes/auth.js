f2ef01ffbbe12d321c4bc595768fdd1595c15360
Suraj kushwaha <skk74143@gmail.com>
2022-02-07 15:07:29 +0530

    Added Login componet

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -56,6 +56,7 @@ router.post('/login', [
     body('email', 'Enter a valid name').isEmail(),
     body('password', 'Password cannot be blank').exists()
 ], async (req, res) => {
+    let success = false;
     // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -70,7 +71,8 @@ router.post('/login', [
         }
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
-            return res.status(400).json({ error: "Please try to login with correct credentials" });
+            success = false;
+            return res.status(400).json({ success,error: "Please try to login with correct credentials" });
         }
         const data = {
             user: {
@@ -78,7 +80,8 @@ router.post('/login', [
             }
         }
         const authToken = jwt.sign(data, JWT_SECRET)
-        res.json({ authToken })
+        success = true;
+        res.json({ success,authToken })
     } catch (error) {
         console.log(error.message);
         res.status(500).send("Internal server error");
---
+++ src/components/Signup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Signup = () => {
+  return <div>
+      i am Signup
+  </div>;
+};
